29170ca8da468f4a0b88695af0aafce5094531b1
Manny Rodriguez <mannyrz.aim@outlook.com>
2017-08-29 19:17:06 -0600

    Initial Commit

---
+++ RESTfulExample/deploy.sh
@@ -0,0 +1,9 @@
+
+ls target/RESTfulExample.war
+ls /cygdrive/c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 7.0/bin/webapps/
+
+cp target/RESTfulExample.war /cygdrive/c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 7.0/bin/webapps/
+
+ 
+ls -l /cygdrive/c/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 7.0/bin/webapps/
+
